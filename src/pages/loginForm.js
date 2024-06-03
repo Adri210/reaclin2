ea002d02f59a969c583f085d65c496dc14168c3f
PedroLucas03 <engs-pedrolubina@camporeal.edu.br>
2024-06-03 08:01:27 -0300

    Initial commit

---
+++ src/pages/loginForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../styles/index.css';
+import '../styles/loginForm.css';
+import logo from '../imagens/logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  return (
+    <body>
+    <main className="d-flex h-100">
+      <form className="formulario">
+        <img src={logo} alt="Logotipo Real Clin" className="mobile-logo" />
+        <h1>Entrar</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <a href="#" className="link-dois">Esqueci minha senha</a>
+        <Link to='/Usuario' className="botao-entrar">Entrar</Link>
+        <div className="paragrafo">
+          <p className="m-0">Não tem uma conta?</p>
+          <a href="#">Cadastrar</a>
+        </div>
+      </form>
+      <div className="logo">
+        <img src={logo} alt="Logotipo Real Clin" />
+      </div>
+    </main>
+    </body>
+  );
+};
+
+export default LoginForm;
